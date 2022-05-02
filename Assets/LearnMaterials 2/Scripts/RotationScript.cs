96c192369bf8192dca1c81e2a067f5a261d2da0e
Alrandis <79513543+Alrandis@users.noreply.github.com>
2022-05-02 19:30:47 +0700

    Создал скрипты
    Создал MoveScript, SampleScript, RotationScript. Изменил TransparentModule

---
+++ Assets/LearnMaterials 2/Scripts/RotationScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : SampleScript
+{
+    private bool startProgramm;
+    public float speed = 0;
+    public float xAngle;
+    public float rotate;
+    public Transform beginRotation;
+
+    [ContextMenu("Запуск программы Rotation")]
+    new public void Use()
+    {
+        startProgramm = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startProgramm)
+        {
+            if (xAngle < rotate)
+            {
+                xAngle += Time.deltaTime * speed;
+                transform.rotation = Quaternion.Euler(xAngle, 0, 0);
+            }
+        }
+    }
+}
