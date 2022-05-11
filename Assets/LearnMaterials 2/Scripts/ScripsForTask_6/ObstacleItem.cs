7bb89f178cdf8e02f060c23f62eaceb85edc1379
Alrandis <79513543+Alrandis@users.noreply.github.com>
2022-05-11 20:12:02 +0700

    Подготовил сцену для 6 задания
    Создал плоскость с нужным тегом. Создал пустые скрипты. Создал объекты на сцене и тд.

---
+++ Assets/LearnMaterials 2/Scripts/ScripsForTask_6/ObstacleItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ObstacleItem : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onDestroyObstacle;
+    // Здесь допиши код
+}
